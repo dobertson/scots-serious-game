5a61a6d4c6f5cc4346d59d8f0a2bca67b48e5ef6
Darren Robertson <darrenjamesrobertson@gmail.com>
2020-11-20 15:53:41 +0000

    modelled and arranged Scuil scene

--- Assets/_Scripts/InsertDialogue.cs
+++ Assets/_Scripts/InsertDialogue.cs
@@ -6,29 +6,48 @@ using UnityEngine.UI;
 public class InsertDialogue : MonoBehaviour
 {
     public Transform textArea;
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI textPrefab;
+    public TextMeshProUGUI speakerPrefab;
+    public string currentlySpeaking;
 
     public void CreateText(string line)
     {
-        string[] splittedParams = line.Split('@');
-        Debug.Log(splittedParams);
-        var newText = Instantiate(text, textArea);
-        newText.GetComponent<DialogueText>().SetValues(
-            splittedParams[0],
-            splittedParams[1],
-            FindObjectOfType<DialogueTranslated>().Get(splittedParams[0]));
+        string[] splitParams = line.Split('@');
 
-        //StartCoroutine(ScrollToBottom());
+        var lineId = splitParams[0];
+        var splitText = splitParams[1].Split(':');
+        var speaker = splitText[0];
+        var text = splitText[1];
+        var translatedText = FindObjectOfType<DialogueTranslated>().Get(splitParams[0]);
+
+        if (!speaker.Equals(currentlySpeaking))
+        {
+            currentlySpeaking = splitText[0];
+            var newSpeaker = Instantiate(speakerPrefab, textArea);
+            newSpeaker.GetComponent<TextMeshProUGUI>().text = currentlySpeaking;
+            newSpeaker.transform.SetSiblingIndex(textArea.transform.childCount - 2);
+        }
+
+        var newText = Instantiate(textPrefab, textArea);
+        newText.GetComponent<DialogueText>().SetValues(
+            lineId,
+            text,
+            translatedText);
+
+        StartCoroutine(ScrollToBottom());
     }
 
+    // once text has added, scroll to bottom of text area 
     IEnumerator ScrollToBottom()
     {
-        yield return new WaitForEndOfFrame(); 
+        yield return new WaitForEndOfFrame();
+        Canvas.ForceUpdateCanvases();
         FindObjectOfType<ScrollRect>().verticalNormalizedPosition = 0;
     }
 
     public void ClearText()
     {
+        currentlySpeaking = "";
         for (int i = 0; i < textArea.transform.childCount - 1; i++)
         {
             Destroy(textArea.GetChild(i).gameObject);
